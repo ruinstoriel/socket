d977165e58f95c7bef51b936d04e8fef941fb951
toriel <ruinstoriel@proton.me>
2024-04-05 19:59:27 +0800

    https 完成

---
+++ mitm/cache.go
@@ -0,0 +1,44 @@
+// Copyright 2018 ouqiang authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+// Package cert HTTPS证书
+package main
+
+import (
+	"crypto/tls"
+	"sync"
+)
+
+// Cache 证书缓存接口
+type Cache interface {
+	Set(host string, c *tls.Certificate)
+	Get(host string) *tls.Certificate
+}
+
+// 实现证书缓存接口
+type CacheImp struct {
+	m sync.Map
+}
+
+func (c *CacheImp) Set(host string, cert *tls.Certificate) {
+	c.m.Store(host, cert)
+}
+func (c *CacheImp) Get(host string) *tls.Certificate {
+	v, ok := c.m.Load(host)
+	if !ok {
+		return nil
+	}
+
+	return v.(*tls.Certificate)
+}
--- mitm/server.go
+++ mitm/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"time"
 )
@@ -27,6 +29,13 @@ func CopyHeader(dst, src http.Header) {
 	}
 }
 
+var badGateway = []byte(fmt.Sprintf("HTTP/1.1 %d %s\r\n\r\n", http.StatusBadGateway, http.StatusText(http.StatusBadGateway)))
+var tunnelEstablishedResponseLine = []byte("HTTP/1.1 200 Connection established\r\n\r\n")
+
+type ConnBuffer struct {
+	net.Conn
+	buf *bufio.ReadWriter
+}
 type Proxy struct {
 }
 
@@ -72,10 +81,107 @@ func httpHandle(rw http.ResponseWriter, req *http.Request) {
 	_, _ = io.CopyBuffer(rw, resp.Body, buf)
 }
 
+var cache = &CacheImp{}
+
 func (p Proxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodConnect {
-		// 非隧道代理
-		httpHandle(rw, req)
+	fmt.Println("Method---------------" + req.Method)
+	if req.Method == http.MethodConnect {
+		if hijacker, ok := rw.(http.Hijacker); ok {
+
+			conn, buf, err := hijacker.Hijack()
+			if err != nil {
+				fmt.Printf("hijacker错误: %s", err)
+			}
+			if buf == nil {
+				buf = bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+			}
+			// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nContent-Type: text/html\r\n<html><head><title>Hello World</title></head><h1>Hello World</h1></html>"))
+			conn.Write(tunnelEstablishedResponseLine)
+			clientConn := &ConnBuffer{
+				Conn: conn,
+				buf:  buf,
+			}
+			defer func() {
+				_ = clientConn.Close()
+			}()
+
+			cert := NewCertificate(cache, true)
+			fmt.Println("host-------", req.URL.Host)
+			tlsConfig, err := cert.GenerateTlsConfig(req.URL.Host)
+			if err != nil {
+				fmt.Printf("%s - HTTPS解密, 生成证书失败: %s", req.URL.Host, err)
+				return
+			}
+			tlsClientConn := tls.Server(clientConn, tlsConfig)
+
+			defer func() {
+				_ = tlsClientConn.Close()
+			}()
+			if err := tlsClientConn.Handshake(); err != nil {
+				fmt.Printf("%s - HTTPS解密, 握手失败: %s \n", req.URL.Host, err)
+				return
+			}
+			buff := bufio.NewReader(tlsClientConn)
+			tlsReq, err := http.ReadRequest(buff)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Printf("%s - HTTPS解密, 读取客户端请求失败: %s", req.URL.Host, err)
+				}
+				return
+			}
+			tlsReq.RemoteAddr = req.RemoteAddr
+			fmt.Println("RemoteAddr---------------" + tlsReq.URL.Host)
+			tlsReq.URL.Scheme = "https"
+			tlsReq.URL.Host = tlsReq.Host
+			fmt.Println("Host---------------" + tlsReq.URL.Host)
+			// ipv6可能导致错误，非标准端口可能导致错误
+			targetAddr := net.JoinHostPort(req.URL.Host, req.URL.Port())
+			fmt.Println(targetAddr)
+			fmt.Println("targetAddr---------------" + targetAddr)
+
+			newReq := new(http.Request)
+			*newReq = *tlsReq
+			newHeader := http.Header{}
+			CloneHeader(newReq.Header, newHeader)
+			newReq.Header = newHeader
+			fmt.Println(newReq.Header)
+			for _, item := range hopHeaders {
+				if newReq.Header.Get(item) != "" {
+					newReq.Header.Del(item)
+				}
+			}
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+				MaxIdleConns:          100,
+				MaxConnsPerHost:       10,
+				IdleConnTimeout:       10 * time.Second,
+				TLSHandshakeTimeout:   5 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+			}
+			fmt.Println(newReq.Header)
+			resp, err := tr.RoundTrip(newReq)
+			if err == nil {
+				for _, h := range hopHeaders {
+					resp.Header.Del(h)
+				}
+			}
+			if err != nil {
+				fmt.Errorf("%s - HTTPS解密, 请求错误: %s", newReq.URL, err)
+				_, _ = tlsClientConn.Write(badGateway)
+				return
+			}
+			err = resp.Write(tlsClientConn)
+			if err != nil {
+				fmt.Errorf("%s - HTTPS解密, response写入客户端失败, %s", req.URL, err)
+			}
+			_ = resp.Body.Close()
+
+		} else {
+			httpHandle(rw, req)
+
+		}
 	}
 }
 
